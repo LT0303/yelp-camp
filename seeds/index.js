49a3095f0cc29af9b6b37fb2fe063f98dceb2ddf
L. Thackeray <lthackeray33@gmail.com>
2023-08-13 19:34:41 -0400

    Created loop within seedDB to seed database with 50 random new camps

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const cities = require('./cities');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -11,8 +12,13 @@ db.once("open", () => {
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    const c = new Campground({title: 'purple fields'});
-    await c.save();
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save();
+    }
 }
 
 seedDB();
