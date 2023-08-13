c7cc87351640577ea7120c2e972375b5fd55b768
L. Thackeray <lthackeray33@gmail.com>
2023-08-13 19:27:28 -0400

    Fixed Campground model path

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
