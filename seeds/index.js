293d76fc917498beb1a100a5c465f99f48011769
L. Thackeray <lthackeray33@gmail.com>
2023-08-26 21:16:44 -0400

    Added price seed and image to new campground seeds

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,16 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            price: price,
+            description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Saepe, maiores? Nisi eos sequi architecto fugit quo explicabo, quaerat 
+            asperiores ullam sed beatae cumque natus eum ipsa tempora itaque, veritatis 
+            est!`
         })
         await camp.save();
     }
